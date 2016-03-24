e97ff384ff1960465d8fc294b21b2ff8e2aa02be
athorhallsson <athorhallsson@gmail.com>
2016-03-24 21:26:32 +0000

    Random error added to all db tables

--- python/cases_insert.py
+++ python/cases_insert.py
@@ -48,7 +48,11 @@ for i in range(len(diseases)):
         posQuery = posQuery[:-2]
         posQuery += ") VALUES (" + str(2 * id) + ", "
         for _ in pos:
-            posQuery += "true, "
+            rand = random.randint(1, 100)
+            if rand < errorRate:
+                posQuery += "false, "
+            else:
+                posQuery += "true, "
         posQuery = posQuery[:-2]
         posQuery += ");\n"
         f_new.write(posQuery)
@@ -61,7 +65,11 @@ for i in range(len(diseases)):
         rposQuery = rposQuery[:-2]
         rposQuery += ") VALUES (" + str(2 * id + 1) + ", "
         for _ in referredPos:
-            rposQuery += "true, "
+            rand = random.randint(1, 100)
+            if rand < errorRate:
+                rposQuery += "false, "
+            else:
+                rposQuery += "true, "
         rposQuery = rposQuery[:-2]
         rposQuery += ");\n"
         f_new.write(rposQuery)
@@ -74,7 +82,11 @@ for i in range(len(diseases)):
         painQuery = painQuery[:-2]
         painQuery += ") VALUES (" + str(2 * id) + ", " + str(2 * id) + ", " + str(2 * id + 1) + ", "
         for _ in pains:
-            painQuery += "true, "
+            rand = random.randint(1, 100)
+            if rand < errorRate:
+                painQuery += "false, "
+            else:
+                painQuery += "true, "
         painQuery = painQuery[:-2]
         painQuery += ");\n"
         f_new.write(painQuery)
