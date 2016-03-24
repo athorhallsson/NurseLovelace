fe4ecf5ec993accd2512fb14c140cc80cfcf6119
athorhallsson <athorhallsson@gmail.com>
2016-03-24 14:03:37 +0000

    Permutations of false values added to python script

--- python/cases_insert.py
+++ python/cases_insert.py
@@ -8,7 +8,7 @@ dName = str(sys.argv[1])
 dId = int(sys.argv[2])
 
 # Initialize random number generator
-random.seed(dId)
+random.seed(dName)
 
 f_old = open(dName + ".txt")
 f_new = open("fill_" + dName + ".txt", "w")
@@ -16,41 +16,50 @@ f_new = open("fill_" + dName + ".txt", "w")
 lines = f_old.readlines()
 
 diagnosis = lines[0]
-malePercent = int(lines[1])
-minAge = int(lines[2])
-maxAge = int(lines[3])
-majorSymptoms = lines[4].split()
-minorSymptoms = lines[5].split()
+majorSymptoms = lines[1].split()
+minorSymptoms = lines[2].split()
+malePercent = int(lines[24])
 
-for _ in range(50):
-    age = random.randint(minAge, maxAge)
-    randomGender = random.randint(1, 100)
-    if randomGender <= malePercent:
-        gender = "'M'"
-    else:
-        gender = "'F'"
+# Make age list
+ages = []
+min = 0
+for n in range(3, 23):
+	for c in range(int(lines[n])):
+        	ages.append(random.randint(min + 1, min + 5))
+    	min += 5
 
-	query = "INSERT INTO Cases (diagnosisId, age, gender, "
-
-	for symptom in majorSymptoms:
-		query += symptom + ", "
-    for symptom2 in minorSymptoms:
-        query += symptom2 + ", "
+for ageIndex in range(100):
+	randomGender = random.randint(1, 100)
+	if randomGender <= malePercent:
+       		gender = "'M'"
+    	else:
+        	gender = "'F'"
+    	query = "INSERT INTO Cases (diagnosisId, age, gender, "
+    	for symptom in majorSymptoms:
+       		query += symptom + ", "
+    	for symptom2 in minorSymptoms:
+		query += symptom2 + ", "
 	# delete last two characters
 	query = query[:-2]
 
-	query += ") VALUES ("
-	query += str(dId) + ", "
-	query += str(age) + ", "
-	query +=  gender + ", "
+    	query += ") VALUES ("
+    	query += str(dId) + ", "
+    	query += str(ages[ageIndex]) + ", "
+    	query +=  gender + ", "
 
-	for _ in majorSymptoms:
-		query += "true, "
-    for _ in minorSymptoms:
-        query += "true, "
-    # delete last two characters
-    query = query[:-2]
-    query += ");\n"
-    f_new.write(query)
-        	
-
+    	for _ in majorSymptoms:
+		rand = random.randint(1, 10)
+		if rand is 10:
+			query += "false, "
+		else:
+			query += "true, "
+    	for _ in minorSymptoms:
+		rand = random.randint(1, 10)
+		if rand is 10:
+			query += "false, "
+		else:
+			query += "true, "
+    	# delete last two characters
+    	query = query[:-2]
+    	query += ");\n"
+    	f_new.write(query)
