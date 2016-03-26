774db82185c6afbc704606cdb514685e87e33f60
Ásgeir Másson <asgeirthor@gmail.com>
2016-03-26 23:58:17 +0000

    Added tables to keep pain sx names and positions, change fill script for that, changed search algorithm to take in mainSx and pain info and use as init search space, changed CBR to gather base cases via age and gender.

--- python/simple_insert.py
+++ python/simple_insert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-with open("Symptoms50.txt") as f_old, open("Fill.txt", "w") as f_new:
+with open("painPos.txt") as f_old, open("Fill_painPos.txt", "w") as f_new:
     for line in f_old:
 	line = line.replace('\n', '').replace('\r', '')
-        f_new.write("INSERT INTO Symptoms (sName) VALUES ('" + line + "');\n")
+        f_new.write("INSERT INTO PainPosition (ppName) VALUES ('" + line + "');\n")
